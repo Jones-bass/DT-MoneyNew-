9400b57b58445225260e1bf1d97d57c55a2440fb
jones-bass <jonesbass.tb@gmail.com>
2022-08-30 17:08:41 -0300

    TrasactionsCard

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionContainer, TransactionTable } from "./styles";
 
 
 export function Transaction() {
@@ -7,6 +8,32 @@ export function Transaction() {
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>     
+                <PriceHighLight variant="income">
+                R$ 12.000,00
+              </PriceHighLight></td>
+              <td>Venda</td>
+              <td>13/08/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  R$ -59,00
+                </PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/08/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
