da231e7fc4a2f03f88f931f665711af735634700
jones-bass <jonesbass.tb@gmail.com>
2022-09-08 22:34:38 -0300

    JsonServer e Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transaction } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  return(
+  return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle/>
-     <Transaction/>
+      <GlobalStyle />
+
+      <TransactionsProvider>
+        <Transaction />
+      </TransactionsProvider>
     </ThemeProvider>
-  )  
+  );
 }
