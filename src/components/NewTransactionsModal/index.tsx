2a3e41d21b6d71c45e638d3da4335b36b8828b35
jones-bass <jonesbass.tb@gmail.com>
2022-09-14 20:58:31 -0300

    React-Hook-Form

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,5 +1,8 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as Dialog from "@radix-ui/react-dialog";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { Controller, useForm } from "react-hook-form";
+import * as z from 'zod'
 import {
   Overlay,
   Content,
@@ -8,7 +11,35 @@ import {
   TransactionTypeButton,
 } from "./styles";
 
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
+
 export function NewTransactionsModal() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: {
+      type: 'income',
+    },
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(data);
+
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -20,23 +51,49 @@ export function NewTransactionsModal() {
           <X size={24} />
         </CloseButton>
 
-        <form action="">
-          <input type="type" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="type" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input 
+            type="type" 
+            placeholder="Descrição"
+            required 
+            {...register('description')} />
+          
+          <input 
+            type="number" 
+            placeholder="Preço" 
+            required 
+            {...register('price', { valueAsNumber: true })} />
 
-          <TransactionType>
-            <TransactionTypeButton variant='income' value="income">
-              <ArrowCircleUp size={24} />
-              Entrada
-            </TransactionTypeButton>
-            <TransactionTypeButton variant='outcome' value="outcome">
-              <ArrowCircleDown size={24} />
-              Saída
-            </TransactionTypeButton>
-          </TransactionType>
+          <input 
+            type="type" 
+            placeholder="Categoria" 
+            required 
+            {...register('category')} />
 
-          <button type="submit">Cadastrar</button>
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              return (
+                <TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+                  
+                  <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
+          />
+
+          <button type="submit" disabled={isSubmitting}>Cadastrar</button>
         </form>
       </Content>
     </Dialog.Portal>
--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -30,8 +30,14 @@ export const SearchFormContainer = styled.form`
     color: ${props => props.theme.green300};
     font-weight: bold;
     border-radius: 6px;
+    cursor: pointer;
 
-    &:hover{
+    &:disabled {
+      opacity: 0.7;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover{
       background: ${props => props.theme.green500};
       border: 1px solid ${props => props.theme.green500};
       color: ${props => props.theme.white};
